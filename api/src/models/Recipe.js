8bd8c05c1f5ce853d32cc53b5cb1e72149922d36
Lihue Maidana Avaca <lihuevida@gmail.com>
2021-09-28 13:19:34 -0300

    1.0.3

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,23 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Recipe', {
-    id : {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },  
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      score: {
+        type: DataTypes.INTEGER,
+      },
+      healthiness: {
+        type: DataTypes.INTEGER,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+      steps: {
+        type: DataTypes.TEXT,
+      },
+      created: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-    resume: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    healthScore: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    
+  );
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require ('axios')
+const { Recipe, Type } = require ('../db')
 
 
 const router = Router();
@@ -8,5 +10,59 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const getApiInfo = async () => {
+    const apiUrl = await axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=ecce4fd21f8b43138d926e3c808887b2&addRecipeInformation=true&number=100');
+    const apiInfo = await apiUrl.data.results.map(recipe => {
+        return {
+            title: recipe.title,
+            id: recipe.id,
+            created: false,
+            Diets: recipe.diets.map((diet) => {return { name: diet };}),
+            dishTypes: recipe.dishTypes,
+            healthScore: recipe.healthScore,
+            score: parseInt(recipe.spoonacularScore),
+            summary: recipe.summary,
+            image: recipe.image,
+            steps: recipe.analyzedInstructions.map((r) => r.steps.map((s) => s.step)).flat(2).join(""),
+        };
+    });
+    return apiInfo;
+};
+
+const getDbInfo = async () => {
+    return await Recipe.findAll({
+        include: {
+            model: Type,
+            through: {
+                attributes: [],
+            },
+        }
+    })
+}
+
+const getAllRecipe = async () => {
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    const infoTotal = dbInfo.concat(apiInfo);
+    return infoTotal
+}
+
+router.get('/recipes', async (req, res) => {
+    const name = req.query.name
+    
+    let recipesTotal = await getAllRecipe();
+    if(name){
+        console.log('acawachin',recipesTotal)
+        let recipeTitle = await recipesTotal.filter(recipe => recipe.title.toLowerCase().includes(name.toLowerCase()))
+        
+        recipeTitle.length ?
+        res.status(200).send(recipeTitle) :
+        res.status(404).send('eso nourecipi papi.juju')
+    } else{
+        res.status(200).send(recipesTotal)
+    }
+})
+
 
 module.exports = router;
+
